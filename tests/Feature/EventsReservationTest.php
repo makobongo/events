c22ce57f70fa71f26e83cc880951074cf4f20dab
Steve Obongo <steveobongo0@gmail.com>
2024-08-13 13:04:29 +0300

    update: method to test the deletion of  events

--- tests/Feature/EventsReservationTest.php
+++ tests/Feature/EventsReservationTest.php
@@ -13,14 +13,14 @@ class EventsReservationTest extends TestCase
     /**
      * @test
      */
-    public function event_can_be_added_to_calender()
+    public function events_can_be_added()
     {
         $response = $this->post('/events', [
             'title' => 'meeting on the lakeside',
             'publisher' => 'makobongo'
         ]);
-        $response->assertOk();
         $this->assertCount(1, Event::all());
+        $response->assertRedirect('/events');
     }
 
     /**
@@ -57,12 +57,30 @@ class EventsReservationTest extends TestCase
             'publisher' => 'Tom Okeyo'
         ]);
         $event = Event::first();
-        $response = $this->patch('/events/'.$event->id, [
+        $response = $this->patch('/events/' . $event->id, [
             'title' => 'second more',
             'publisher' => 'Valary Ogada'
         ]);
 
         $this->assertEquals('second more', Event::first()->title);
         $this->assertEquals('Valary Ogada', Event::first()->publisher);
+
+        $response->assertRedirect('/events/' . $event->id);
+    }
+
+    /**
+     * @test
+     */
+    public function event_can_be_deleted()
+    {
+        $this->post('/events', [
+            'title' => 'one more',
+            'publisher' => 'Tom Okeyo'
+        ]);
+        $this->assertCount(1, Event::all());
+        $event = Event::first();
+        $response = $this->delete('/events/' . $event->id);
+        $this->assertCount(0, Event::all());
+        $response->assertRedirect('/events');
     }
 }
