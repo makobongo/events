225b841d928492707d27fed436d17a9d7d949d7c
Steve Obongo <steveobongo0@gmail.com>
2024-08-13 13:06:54 +0300

    update: added method to delete events

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -14,14 +14,24 @@ class EventController extends Controller
             'publisher' => 'required'
         ]);
         Event::create($data);
+        return redirect('/events');
     }
 
-    public function update(Event $event) {
+    public function update(Event $event)
+    {
         $data = request()->validate([
             'title' => 'required',
             'publisher' => 'required'
         ]);
 
         $event->update($data);
+
+        return redirect('/events/' . $event->id);
+    }
+
+    public function destroy(Event $event)
+    {
+        $event->delete();
+        return redirect('/events');
     }
 }
