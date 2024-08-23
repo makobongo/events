e3648eba7dbaaebd6a8f5c916a2e9e2e6aadc465
Steve Obongo <steveobongo0@gmail.com>
2024-08-23 12:11:48 +0300

    update: added prices variable for display

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Price;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $prices = Price::all();
+        return view('home', ['prices'=>$prices]);
     }
 }
