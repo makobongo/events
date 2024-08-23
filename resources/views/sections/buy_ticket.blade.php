e414310783fa9a607e6e0604c5e866f6e0b816c5
Steve Obongo <steveobongo0@gmail.com>
2024-08-23 12:14:44 +0300

    update: added prices to render

--- resources/views/sections/buy_ticket.blade.php
+++ resources/views/sections/buy_ticket.blade.php
@@ -3,61 +3,63 @@
 
     <div class="section-header">
       <h2>Buy Tickets</h2>
-      <p>Velit consequatur consequatur inventore iste fugit unde omnis eum aut.</p>
+      <p>Buy tickets for the sixx spirits event</p>
     </div>
 
     <div class="row">
-        <div class="col-lg-4">
-          <div class="card mb-5 mb-lg-0">
-            <div class="card-body">
-              <h5 class="card-title text-muted text-uppercase text-center">name</h5>
-              <h6 class="card-price text-center">price</h6>
-              <hr>
-              <ul class="fa-ul">
-                  <li>
-                    <span class="fa-li"><i class="fa fa-"></i></span>name
-                  </li>
-              </ul>
-              <hr>
-              <div class="text-center">
-                <button type="button" class="btn" data-toggle="modal" data-target="#buy-ticket-modal" data-ticket-type="">Buy Now</button>
-              </div>
+    @foreach($prices as $price)
+      <div class="col-lg-4">
+        <div class="card mb-5 mb-lg-0">
+          <div class="card-body">
+            <h5 class="card-title text-muted text-uppercase text-center">{{ $price->name }}</h5>
+            <h6 class="card-price text-center">Ksh {{ $price->price }}</h6>
+            <hr>
+            <ul class="fa-ul">
+              <li>
+                <span class="fa-li"><i class="fa fa-"></i></span>name
+              </li>
+            </ul>
+            <hr>
+            <div class="text-center">
+              <button type="button" class="btn" data-toggle="modal" data-target="#buy-ticket-modal" data-ticket-type="">Buy Now</button>
             </div>
           </div>
         </div>
-  </div>
+      </div>
+      @endforeach
+    </div>
 
-  <!-- Modal Order Form -->
-  <div id="buy-ticket-modal" class="modal fade">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title">Buy Tickets</h4>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          <form method="POST" action="#">
-            <div class="form-group">
-              <input type="text" class="form-control" name="your-name" placeholder="Your Name">
-            </div>
-            <div class="form-group">
-              <input type="text" class="form-control" name="your-email" placeholder="Your Email">
-            </div>
-            <div class="form-group">
-              <select id="ticket-type" name="ticket-type" class="form-control" >
-                <option value="">-- Select Your Ticket Type --</option>
+    <!-- Modal Order Form -->
+    <div id="buy-ticket-modal" class="modal fade">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h4 class="modal-title">Buy Tickets</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form method="POST" action="#">
+              <div class="form-group">
+                <input type="text" class="form-control" name="your-name" placeholder="Your Name">
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control" name="your-email" placeholder="Your Email">
+              </div>
+              <div class="form-group">
+                <select id="ticket-type" name="ticket-type" class="form-control">
+                  <option value="">-- Select Your Ticket Type --</option>
                   <option value="#">name</option>
-              </select>
-            </div>
-            <div class="text-center">
-              <button type="submit" class="btn">Buy Now</button>
-            </div>
-          </form>
-        </div>
-      </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-  </div><!-- /.modal -->
+                </select>
+              </div>
+              <div class="text-center">
+                <button type="submit" class="btn">Buy Now</button>
+              </div>
+            </form>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
 
 </section>
