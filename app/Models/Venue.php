76ae7292054230ab96d3ccd8347c2ebb343b3e7b
Steve Obongo <steveobongo0@gmail.com>
2024-08-23 16:53:33 +0300

    feat: added new models Gallery, Speaker and Venue

---
+++ app/Models/Venue.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Venue extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'address',
+        'latitude',
+        'longitude',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'description',
+    ];
+}
