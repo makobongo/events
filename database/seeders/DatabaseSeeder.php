e402b584017cb41899ffe76e4b8f2ae64828929f
Steve Obongo <steveobongo0@gmail.com>
2024-08-23 13:51:01 +0300

    feat: created migration for amenities and pivot table for amenities price and respective seeders'

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            PricesTableSeeder::class
+            PricesTableSeeder::class,
+            AmenitiesTableSeeder::class,
+            AmenityPriceTableSeeder::class,
         ]);
     }
 }
