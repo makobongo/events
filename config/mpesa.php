a091a73e60892dad0da7ee937b0b9cbdaf44d87e
Steve Obongo <steveobongo0@gmail.com>
2024-08-24 11:29:40 +0300

    feat: added configuration for mpesa environment

---
+++ config/mpesa.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'credentials' => [
+        'consumer_key' => env('MPESA_CONSUMER_KEY'),
+        'consumer_secret' => env('MPESA_CONSUMER_SECRET'),
+        'mpesa_env' => env('MPESA_ENV')
+    ]
+
+];
