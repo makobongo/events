9635737379cd37c8da0742247a354782740b8683
Steve Obongo <steveobongo0@gmail.com>
2024-08-26 15:00:52 +0300

    update: added csrf except urls

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        '/initiate_payment',
+        '/stk_callback'
     ];
 }
