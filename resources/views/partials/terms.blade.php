63f85cfee5d2419fcf0cb999a5b9aaefca2bc457
Steve Obongo <steveobongo0@gmail.com>
2024-08-27 10:16:37 +0300

    feat: added terms and conditions blade

---
+++ resources/views/partials/terms.blade.php
@@ -0,0 +1,53 @@
+<div id="accordion">
+    <div class="card">
+        <div class="card-header" id="headingTwo">
+            <input class="form-check-input" type="checkbox" value="" required>
+            <div class="alert alert-danger" role="alert" class="mb-0" id="defaultCheck1" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Terms & Conditions apply
+            </div>
+        </div>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+            <div class="card-body">
+                These Terms & Conditions, the “Organiser”, “Us”, “Our”, or “We” means the event or show printed on this ticket.<br/>
+
+                1.Tickets to this event are subject to the following terms and conditions. Any attendee of the Event agrees to the terms and conditions outlined by our ticket.<br/>
+
+                2. This event is PRIVATE and on invitation ONLY. Entry to the Event is for above 18 years of age with a valid ticket or pass.<br/>
+
+                3. For the safety of people at the Event, the venue management and the Organiser reserve the right to refuse admission and conduct security searches of vehicles and persons as they deem appropriate. Event Attendees will be expected to cooperate accordingly.<br/>
+
+                4. All tickets should be checked on purchase, as mistakes cannot always be rectified.<br/>
+
+                5. Tickets cannot be exchanged or refunded after purchase. Duplicate tickets cannot be issued under any circumstances.<br/>
+
+                6. In addition to the ticket price, your order may require payment of a booking fee per ticket, a transaction fee per order and/or other supplementary fees which may apply. Those fees are not refundable.<br/>
+
+                7. If the Event is rescheduled or moved, the Organiser will give you the options of either retaining or exchanging your tickets for the new date/location. If the Event is cancelled, you may be offered the option to transfer your ticket to a future event. Please note that any Booking Fees and/or Transaction Fees are not refundable in these circumstances.<br/>
+
+                8. If the Event is rescheduled, changed, moved or cancelled the Organiser cannot be held responsible for any resulting costs you may incur for travel, accommodation and other related goods, services or compensation.<br/>
+
+                9. Altered tickets or passes will become void and the holder will be refused admission.<br/>
+
+                10. Tickets cannot be resold for commercial gain. If we have reason to believe that tickets have been re-sold, they will be invalidated, and the holder will be refused admission.<br/>
+
+                11. We may restrict the sale of tickets to a maximum number per person (allocation). We reserve the right to cancel tickets bought in excess of this allocation.<br/>
+
+                12. The Organiser reserves the right to alter or vary the content or timing of any part of the Event in circumstances beyond its reasonable control without any obligation to make any refunds or exchange tickets.<br/>
+
+                13. The Organiser reserves the right to refuse admission otherwise behaving dangerously or inappropriately, or for any other behaviour likely to cause damage, injury, nuisance and annoyance or for failure to comply with the reasonable request of the Organiser.<br/>
+
+                14. Neither the Organiser nor the venue operator will be responsible for any loss, damage, death or injury howsoever caused unless (i) the relevant party has breached its legal obligations; and (ii) such loss or damage is a direct and reasonably foreseeable result of the breach. For the avoidance of doubt, the Organiser will not be responsible for the actions of any third party, including, but not limited to, the service providers. Every person attending the event is required to exercise due care and responsible for their safety and that of their belongings.<br/>
+
+                15. If all or any of the Event is planned to take place outdoors it will take place regardless of weather conditions (unless deemed unsafe by the safety officer). There are no refunds for bad weather (at indoor or outdoor events). We strongly advise that you come dressed appropriately for an outdoor Event.<br/>
+
+                16. The Organiser does not accept any responsibility for any loss or damage to personal property brought to the Event.<br/>
+
+                17. By attending this event you give consent are in the full knowledge that Visitors may be filmed, photographed or sound recorded for broadcast, publication or security purposes whilst at the Event by the Organiser and our trusted sponsors and partners.<br/>
+
+                18. Visitors are not permitted to bring animals, food, drink either non alcoholic or alcoholic and any illegal substances for recreational use to the Event.<br/>
+
+                19. Please check Event details, timings and car park charges prior to travelling as these may be subject to change.
+            </div>
+        </div>
+    </div>
+</div>
