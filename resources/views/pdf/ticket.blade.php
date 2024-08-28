c9d9e24bfdd88e9081e4e0383d175751e0d8fa87
steve obongo <steveobongo0@gmail.com>
2024-08-28 23:00:06 +0300

    update: added text to render

--- resources/views/pdf/ticket.blade.php
+++ resources/views/pdf/ticket.blade.php
@@ -18,22 +18,26 @@
         }
         .content {
             font-size: 12px;
+            margin-left: 30%;
         }
     </style>
 </head>
 <body>
     <div class="container">
         <div class="header">
-            <h1 style="font-family: 'Courier New', monospace; font-size:70px;">{{ $title }}</h1>
-            <p>Date: {{ $date }}</p>
+            <h1 style="font-family: 'Courier New', monospace; font-size:40px;">{{ $title }}</h1>
+            <p>{!! QrCode::size(100)->generate('Demo') !!}</p>
+            <p>Name: {{ $first_name }} {{ $second_name }}</p>
+            <p>Phone: {{ $phone }}</p>
+            <p>Ticket Number: {{ $ticket_code }}</p>
         </div>
         <div class="content">
             <p>Terms & Condition apply</p>
             <p>
-                <ul>
-                    <li>0716 667 121</li>
-                    <li>sixxsocial@sixx-spirits.com</li>
-                    <li>www.sixx-spirits</li>
+                <ul style="list-style: none;">
+                    <li><b>Phone: </b>0716 667 121</li>
+                    <li><b>Email: </b>sixxsocial@sixx-spirits.com</li>
+                    <li><b>Website: </b>www.sixx-spirits.com</li>
                 </ul>
             </p>
         </div>
