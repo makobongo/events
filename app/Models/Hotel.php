d193db5735d5d3308d853025d194e72e5d7ad86f
Steve Obongo <steveobongo0@gmail.com>
2024-08-29 15:46:32 +0300

    feat: created new model for faq, hotel and setting

---
+++ app/Models/Hotel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Hotel extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'rating',
+        'address',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'description',
+        'img'
+    ];
+}
