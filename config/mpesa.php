e2f1438c33dacb4c973122a28eb9f50ca0e1e3c6
steve obongo <steveobongo0@gmail.com>
2024-08-30 21:12:22 +0300

    refactor: renamed mpesa_url to mpesa_stkpush_url

--- config/mpesa.php
+++ config/mpesa.php
@@ -6,7 +6,7 @@ return [
         'consumer_key' => env('MPESA_CONSUMER_KEY'),
         'consumer_secret' => env('MPESA_CONSUMER_SECRET'),
         'mpesa_env' => env('MPESA_ENV'),
-        'mpesa_url' => env('MPESA_URL'),
+        'mpesa_stkpush_url' => env('MPESA_STKPUSH_URL'),
         'pass_key' => env('MPESA_PASS_KEY'),
         'call_back_url' => env('CALL_BACK_URL')
     ]
