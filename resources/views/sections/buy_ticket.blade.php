4bfe029cca6f79ce0e00b3334d47ec811696bc15
Steve Obongo <steveobongo0@gmail.com>
2024-08-31 13:19:44 +0300

    update: added new input fields

--- resources/views/sections/buy_ticket.blade.php
+++ resources/views/sections/buy_ticket.blade.php
@@ -45,8 +45,21 @@
             <form method="POST" action="{{ route('ticket.store') }}">
               @csrf
               <div class="form-group">
-                <input type="text" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}" name="name" placeholder="Your Name" required autocomplete="name" autofocus>
-                @error('name')
+                <!-- <input type="text" class="form-control" value="" name="" placeholder="First Name">
+                <input type="text" class="form-control" value="" name="" placeholder="Second Name"> -->
+                <div class="row">
+                  <div class="col-sm">
+                  <input type="text" class="form-control" value="" name="" placeholder="First Name">
+                  </div>
+                  <div class="col-sm">
+                  <input type="text" class="form-control" value="" name="" placeholder="Second Name">
+                  </div>
+                </div>
+
+              </div>
+              <div class="form-group">
+                <input type="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" name="email" placeholder="Your Email" required autocomplete="email" autofocus>
+                @error('email')
                 <span class="invalid-feedback" role="alert">
                   <strong>{{ $message }}</strong>
                 </span>
