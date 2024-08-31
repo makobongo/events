832e2ea0cce12d69f7388e0b25e4797d89e63dfc
steve obongo <steveobongo0@gmail.com>
2024-08-31 22:28:01 +0300

    feat: created blade for clients mail

---
+++ resources/views/mail/client.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head></head>
+
+<body style="color:black;">
+    <p>Hello,</p>
+    <p>{{ $response }}</p>
+    <p>Kind Regards,</p>
+    <p><b>{{ config('app.name') }}</b></p>
+</body>
+
+</html>
