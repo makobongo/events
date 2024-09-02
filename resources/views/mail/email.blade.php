aeafd15b02d8ff6e82de5a30c3029574f21bc250
Steve Obongo <steveobongo0@gmail.com>
2024-09-02 11:35:10 +0300

    refactor: added variables to render

--- resources/views/mail/email.blade.php
+++ resources/views/mail/email.blade.php
@@ -4,7 +4,8 @@
 <head></head>
 
 <body style="color:black;">
-    <p>Hello SIXX {{ $content }}</p>
+    <p>Hello SIXX,</p>
+    <p>Name: {{ $client->first_name }} {{$status}}</p>
     <p>Kind Regards,</p>
     <p><b>{{ config('app.name') }}</b></p>
 </body>
