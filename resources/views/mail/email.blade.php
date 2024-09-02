f06758344ab9aabc11f42178937963be3b1e85d5
Steve Obongo <steveobongo0@gmail.com>
2024-09-02 11:45:08 +0300

    test

--- resources/views/mail/email.blade.php
+++ resources/views/mail/email.blade.php
@@ -5,7 +5,7 @@
 
 <body style="color:black;">
     <p>Hello SIXX,</p>
-    <p>Name: {{ $client->first_name }} {{$status}}</p>
+    <p>Name: {{ $d }}</p>
     <p>Kind Regards,</p>
     <p><b>{{ config('app.name') }}</b></p>
 </body>
