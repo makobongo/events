5c30afca4b5120552349dd692ff6ee0e49919fe3
Steve Obongo <steveobongo0@gmail.com>
2024-09-03 14:52:22 +0300

    update: commented out various links

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -18,9 +18,9 @@
         <li><a href="{{ Route::current()->getName() != 'home' ? route('home.index') : '' }}#speakers">Entertainers</a></li>
         <li><a href="{{ Route::current()->getName() != 'home' ? route('home.index') : '' }}#schedule">Schedule</a></li>
         <li><a href="{{ Route::current()->getName() != 'home' ? route('home.index') : '' }}#venue">Venue</a></li>
-        <li><a href="{{ Route::current()->getName() != 'home' ? route('home.index') : '' }}#hotels">Hotels</a></li>
+        <!-- <li><a href="{{ Route::current()->getName() != 'home' ? route('home.index') : '' }}#hotels">Hotels</a></li> -->
         <li><a href="{{ Route::current()->getName() != 'home' ? route('home.index') : '' }}#gallery">Gallery</a></li>
-        <li><a href="{{ Route::current()->getName() != 'home' ? route('home.index') : '' }}#supporters">Sponsors</a></li>
+        <!-- <li><a href="{{ Route::current()->getName() != 'home' ? route('home.index') : '' }}#supporters">Sponsors</a></li> -->
         <li><a href="{{ Route::current()->getName() != 'home' ? route('home.index') : '' }}#contact">Contact</a></li>
         <li class="buy-tickets"><a href="{{ Route::current()->getName() != 'home' ? route('home.index') : '' }}#buy-tickets">Buy Tickets</a></li>
       </ul>
