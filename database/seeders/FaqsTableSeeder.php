4800739cd26d5ffb7274f5e55b78b117203e28ee
Steve Obongo <steveobongo0@gmail.com>
2024-09-03 14:49:31 +0300

    update: changed the questions to seed

--- database/seeders/FaqsTableSeeder.php
+++ database/seeders/FaqsTableSeeder.php
@@ -14,13 +14,35 @@ class FaqsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        foreach(range(1, 6) as $key)
+        $faqs = [
+            [
+                'question'              => 'What kind of Event is the SIXX Social?',
+                'answer'       => 'It is a SIXX Spirits community afternoon and early evening Social Event to socialize and enjoy music with good friends'
+            ],
+            [
+                'question'              => 'How do I pay for the ticket?',
+                'answer'       => 'Following the steps on the website, by providing your name and telephone number you will receive a prompt to make payment via MPESA'
+            ],
+            [
+                'question'              => 'What format is the ticket?',
+                'answer'       => 'Once you complete ticket purchase you will receive a Digital PDF ticket that will emailed to you on the email address provided'
+            ],
+            [
+                'question'              => 'Can I attend the event if I am below 18 years of Age accompanying an Adult?',
+                'answer'       => 'NO.  The event is strictly for Adults over the Age of 18'
+            ],
+            [
+                'question'              => 'Can I resale my event ticket after purchase',
+                'answer'       => 'NO, tickets cannot be resold or redeemed for a refund after purchase '
+            ],
+            [
+                'question'              => 'Are pets allowed?',
+                'answer'       => 'NO Animals are allowed at the event'
+            ]
+        ];
+        foreach($faqs as $faq)
         {
-            $faker = Factory::create();
-            Faq::create([
-                'question' => $faker->sentence,
-                'answer' => $faker->paragraph
-            ]);
+            $speaker = Faq::create($faq);
         }
     }
 }
