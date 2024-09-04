6b366210a65aa7798fe9fdad60faf5558e3bb370
Steve Obongo <steveobongo0@gmail.com>
2024-09-04 16:40:13 +0300

    update: adjusted the number of seed to 14

--- database/seeders/GalleriesTableSeeder.php
+++ database/seeders/GalleriesTableSeeder.php
@@ -13,7 +13,7 @@ class GalleriesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        foreach(range(1,6) as $id)
+        foreach(range(1,14) as $id)
         {
             Gallery::create([
                 'name' => $id.'.jpg'
