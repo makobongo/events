2fc7e9f54d664fe930b74b29d5d447c04735eb59
Steve Obongo <steveobongo0@gmail.com>
2024-09-04 15:06:00 +0300

    update: changed the number of image galleries to seed

--- database/seeders/GalleriesTableSeeder.php
+++ database/seeders/GalleriesTableSeeder.php
@@ -13,7 +13,7 @@ class GalleriesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        foreach(range(1,8) as $id)
+        foreach(range(1,3) as $id)
         {
             Gallery::create([
                 'name' => $id.'.jpg'
