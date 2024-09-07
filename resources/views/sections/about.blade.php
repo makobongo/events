d6ae0035df86f738eaa474145a83a321d2b8b03e
Steve Obongo <steveobongo0@gmail.com>
2024-09-07 12:51:20 +0300

    update: adjusted the display

--- resources/views/sections/about.blade.php
+++ resources/views/sections/about.blade.php
@@ -3,15 +3,15 @@
     <div class="row">
       <div class="col-lg-6">
         <h2>About The Event</h2>
-        <p>about description</p>
+        <p>{{ $settings['about_description'] ?? '' }}</p>
       </div>
       <div class="col-lg-3">
         <h3>Where</h3>
-        <p>about where</p>
+        <p>{{ $settings['about_where'] ?? '' }}</p>
       </div>
       <div class="col-lg-3">
         <h3>When</h3>
-        <p>about when</p>
+        <p>{{ $settings['about_when'] ?? '' }}</p>
       </div>
     </div>
   </div>
