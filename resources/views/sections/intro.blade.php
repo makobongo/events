7874030d6f6a27324239f7265f46358010a54c4a
Steve Obongo <steveobongo0@gmail.com>
2024-09-07 13:20:57 +0300

    update: added date info

--- resources/views/sections/intro.blade.php
+++ resources/views/sections/intro.blade.php
@@ -1,7 +1,7 @@
 <section id="intro">
   <div class="intro-container wow fadeIn">
     <h1 class="mb-4 pb-0">{!! $settings['title'] ?? '' !!}</h1>
-    <p class="mb-4 pb-0">{{ $settings['subtitle'] ?? '' }}</p>
+    <p class="mb-4 pb-0">{{ $settings['subtitle'] ?? '' }}, {{ $settings['about_when'] ?? '' }}</p>
     <!-- @if($settings['youtube_link'])
       <a href="{{ $settings['youtube_link'] }}" class="venobox play-btn mb-4" data-vbtype="video"
         data-autoplay="true"></a>
