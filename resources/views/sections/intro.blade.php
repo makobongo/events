1d95b1f8a7385cb6636a417c99decf10be21915d
Steve Obongo <steveobongo0@gmail.com>
2024-09-07 12:13:29 +0300

    update: commented out youtube link

--- resources/views/sections/intro.blade.php
+++ resources/views/sections/intro.blade.php
@@ -2,10 +2,10 @@
   <div class="intro-container wow fadeIn">
     <h1 class="mb-4 pb-0">{!! $settings['title'] ?? '' !!}</h1>
     <p class="mb-4 pb-0">{{ $settings['subtitle'] ?? '' }}</p>
-    @if($settings['youtube_link'])
+    <!-- @if($settings['youtube_link'])
       <a href="{{ $settings['youtube_link'] }}" class="venobox play-btn mb-4" data-vbtype="video"
         data-autoplay="true"></a>
-    @endif
+    @endif -->
     <a href="#about" class="about-btn scrollto">About The Event</a>
   </div>
 </section>
