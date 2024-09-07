c88691bb8bc239756d794fd78f6c4f872d7893bb
Steve Obongo <steveobongo0@gmail.com>
2024-09-07 12:19:05 +0300

    update: adjusted text

--- database/seeders/VenuesTableSeeder.php
+++ database/seeders/VenuesTableSeeder.php
@@ -18,7 +18,7 @@ class VenuesTableSeeder extends Seeder
             'address'       => '00100 Nairobi',
             'latitude'      => '-1.3155358296492337',
             'longitude'     => '36.918241038449764',
-            'description'   =>  'Iste nobis eum sapiente labore.'
+            'description'   =>  '.'
         ]);
     }
 }
