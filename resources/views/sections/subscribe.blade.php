e9b7ea4ea51b1513b040827d7584dee086228c83
Steve Obongo <steveobongo0@gmail.com>
2024-09-09 09:47:35 +0300

    fix: removed post method

--- resources/views/sections/subscribe.blade.php
+++ resources/views/sections/subscribe.blade.php
@@ -5,7 +5,7 @@
       <p>Subscribe to receive regular {{env('APP_NAME')}} updates.</p>
     </div>
 
-    <form method="POST" action="#">
+    <form method="#" action="#">
       <div class="form-row justify-content-center">
         <div class="col-auto">
           <input type="text" class="form-control" placeholder="Enter your Email">
