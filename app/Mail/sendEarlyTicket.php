3ea16471880127bf2a124c2b37413aa82a3dbb99
Steve Obongo <steveobongo0@gmail.com>
2024-09-18 11:44:53 +0300

    feat: created new blade for various tickets

---
+++ app/Mail/sendEarlyTicket.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class sendEarlyTicket extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected $client;
+    // protected $status;
+    protected $attachedFile;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($data, $filePath)
+    {
+        $this->client = $data;
+        // $this->status = $status;
+        $this->attachedFile = $filePath;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: env('APP_NAME')
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.tickets.early',
+            with: ['first_name' => $this->client['first_name'],'last_name' => $this->client['last_name'],'number_of_ticket' => $this->client['number_of_ticket'],'ticket_cost' => $this->client['ticket_cost'],'phone' => $this->client['phone']],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [
+            Attachment::fromPath($this->attachedFile),
+        ];
+    }
+}
---
+++ app/Mail/sendGroupTicket.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class sendGroupTicket extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private $client;
+    private $attachedFile;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($data, $filePath)
+    {
+        $this->client = $data;
+        // $this->status = $status;
+        $this->attachedFile = $filePath;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: env('APP_NAME')
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.tickets.group',
+            with: ['first_name' => $this->client['first_name'],'last_name' => $this->client['last_name'],'number_of_ticket' => $this->client['number_of_ticket'],'ticket_cost' => $this->client['ticket_cost'],'phone' => $this->client['phone']],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [
+            Attachment::fromPath($this->attachedFile),
+        ];
+    }
+}
