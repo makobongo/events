a3d280b7c762ba738f9a1e5d8e1ce1acd23fa629
Steve Obongo <steveobongo0@gmail.com>
2024-09-18 14:28:14 +0300

    update: reverted to original configuration

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -221,8 +221,7 @@ class TicketController extends Controller
     {
         $pdf = PDF::loadView('mail.email', $data)->setPaper([0, 0, 300, 516], 'portrait');
         $pdf->render();
-        Storage::put($data['phone'] . '.pdf', $pdf->output());
-        // file_put_contents($data['phone'] . '.pdf', $pdf->output());
+        file_put_contents($data['phone'] . '.pdf', $pdf->output());
     }
 
     public function stkPush()
