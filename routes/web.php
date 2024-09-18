ab550a130d27dc20db234ca81d671ca87dbbc280
Steve Obongo <steveobongo0@gmail.com>
2024-09-18 11:50:04 +0300

    update: adjusted to encrypt using sha256

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PublisherController;
 use App\Http\Controllers\TicketController;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,6 +29,9 @@ Route::post('stk_callback_url', [TicketController::class, 'lipaNaMpesaCallback']
 Route::post('confirmation', [TicketController::class, 'mpesaConfirmation']);
 Route::post('validation', [TicketController::class, 'mpesaValidation']);
 Route::get('test', function(){
-   return bcrypt('password');
+   // return bcrypt('password');
+   // $sha_phone = Hash::make(254715096287);
+   // return $sha_phone;
+   return hash('sha256', 254715096287);
 });
 // Auth::routes();
